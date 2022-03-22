ea191fdf7f8bb0975082f6475762ba932781092a
cgeshan <76122567+cgeshan@users.noreply.github.com>
2022-03-22 12:30:52 -0400

    Add files via upload

--- code/HC-06 Tests/HC06_Test_Communications.ino
+++ code/HC-06 Tests/HC06_Test_Communications.ino
@@ -1,22 +1,28 @@
 #include <SoftwareSerial.h>
-SoftwareSerial HC06(10,11);
+SoftwareSerial HC05(10,11);
 
-
+char jobs;
 
 void setup() {
   // put your setup code here, to run once:
 
-  HC06.begin(9600);
+  HC05.begin(9600);
+  Serial.begin(9600);
+//  HC05.println("Ready");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(HC06.available() > )){
-    array jobs = HC06.read();
-    HC06.print("SUCCESS!");
-    HC06.println(jobs);
+  if(HC05.available() > 0){
+    Serial.println("YES ");
+    jobs = HC05.read();
+    if(jobs > 0){
+      Serial.print("SUCCESS!");
+    }
+    Serial.println(jobs);
+    HC05.println("Delivered");
   }
 
 }
