43b6b5e2c993566be1b3d97c1ced3b521ba606f3
cgeshan <76122567+cgeshan@users.noreply.github.com>
2022-04-07 14:57:46 -0400

    Update ColorFollowing.ino
    Follows blue painters tape line and stops are red tape. Reads barcode.

--- code/ColorFollowing.ino
+++ code/ColorFollowing.ino
@@ -17,7 +17,6 @@ Pixy2 pixy;
 long maxSize = 0;
 int oldSignature, trackedBlock = 0;
 
-
 // Creating PID Loop
 PIDLoop headingLoop(5000, 0, 0, false);
 
@@ -25,126 +24,118 @@ void setup()
 {
   Serial.begin(115200);
   Serial.print("Starting...\n");
+
   AFMS.begin();
   leftMotor -> run(RELEASE);
-  leftMotor -> setSpeed(0);
   rightMotor -> run(RELEASE);
-
+  Serial.println("Motors On and Set to 0");
+  delay(2000);
+  
   pixy.init();
-  pixy.setLamp(1, 0);  
+  pixy.setLamp(1, 1);  
   //pixy.changeProg("line");
   pixy.changeProg("color");
 
   // look straight and down
-  pixy.setServos(550, 550);
+  pixy.setServos(550, 600);
 }
 void loop()
 {
   int8_t res, blocks;
   int32_t error;
-  int left, right;
   char buf[96];
 
   blocks = pixy.ccc.getBlocks();
   //Serial.println(blocks);
-
-
   if(blocks){
     //Serial.println("We have blocks");
     for (int i = 0; i < blocks; i++){
-      if ((oldSignature == 0) || (pixy.ccc.blocks[i].m_signature == oldSignature)){
-      long newSize = pixy.ccc.blocks[i].m_height * pixy.ccc.blocks[i].m_width;
+      if (pixy.ccc.blocks[i].m_signature == 2){
+        res = pixy.line.getMainFeatures();
+        StationFound(res);  
+      }else if(pixy.ccc.blocks[i].m_signature == 1){
+        long newSize = pixy.ccc.blocks[i].m_height * pixy.ccc.blocks[i].m_width;
         if (newSize > maxSize){
           int trackedBlock = i;
           maxSize = newSize;
         }
-      }
-     // pixy.ccc.blocks[i].print();
-      int xCoord = pixy.ccc.blocks[trackedBlock].m_x;
-      int yCoord = pixy.ccc.blocks[trackedBlock].m_y;
+        int xCoord = pixy.ccc.blocks[trackedBlock].m_x;
+        int yCoord = pixy.ccc.blocks[trackedBlock].m_y;
+        FollowLine(xCoord, yCoord);
+      } 
+    }
+  }else{
+    Serial.println("No Blocks :(");
+    leftMotor -> run(RELEASE);
+    rightMotor -> run(RELEASE);
+  }    
+}
 
-      if(xCoord < (xCenterColor - 10)){
-        left = 75;
-        right = 100;
-        Serial.println("Right");
-      }else if(xCoord > (xCenterColor + 10)){
-        Serial.println("Left");
-        left = 100;
-        right =75;
-      }else if(xCoord < (xCenterColor - 100)){
-        left = 25;
-        right = 100;
-        Serial.println("Right");
-      }else if(xCoord > (xCenterColor + 100)){
-        Serial.println("Left");
-        left = 100;
-        right = 25;
-      }else {
-        left = 100;
-        right = 100;
-        Serial.println("Straight");
-      }  
+int FollowLine(int xCoord, int yCoord){
+  int left, right;
+  Serial.println("Follow Track");
+  if(xCoord < (xCenterColor - 10)){
+      left = 75;
+      right = 100;
       leftMotor -> run(FORWARD);
       leftMotor -> setSpeed(left);
       rightMotor -> run(FORWARD);
       rightMotor -> setSpeed(right);
-      
-/*
-      Serial.print("(");
-      Serial.print(xCoord);
-      Serial.print(", ");
-      Serial.print(yCoord);
-      Serial.print(")     Center of Screen x coordinate: ");
-      Serial.println(xCenterColor);
-*/
+      Serial.println("Right1");
+    }else if(xCoord > (xCenterColor + 10)){
+      Serial.println("Left1");
+      left = 100;
+      right =75;
+      leftMotor -> run(FORWARD);
+      leftMotor -> setSpeed(left);
+      rightMotor -> run(FORWARD);
+      rightMotor -> setSpeed(right);
+    }else if(xCoord < (xCenterColor - 100)){
+      left = 25;
+      right = 100;
+      leftMotor -> run(FORWARD);
+      leftMotor -> setSpeed(left);
+      rightMotor -> run(FORWARD);
+      rightMotor -> setSpeed(right);
+      Serial.println("Right2");
+    }else if(xCoord > (xCenterColor + 100)){
+      Serial.println("Left2");
+      left = 100;
+      right = 25;
+      leftMotor -> run(FORWARD);
+      leftMotor -> setSpeed(left);
+      rightMotor -> run(FORWARD);
+      rightMotor -> setSpeed(right);
+    }else {
+      left = 100;
+      right = 100;
+      leftMotor -> run(FORWARD);
+      leftMotor -> setSpeed(left);
+      rightMotor -> run(FORWARD);
+      rightMotor -> setSpeed(right);
+      Serial.println("Straight");
     }
-  }else{
-    Serial.println("No Blocks :(");
-      leftMotor -> run(RELEASE);
-      rightMotor -> run(RELEASE);
-  }
+}
+
+int8_t StationFound(int8_t res){
   
-/*
-  // Get latest data from Pixy, including main vector, new intersections and new barcodes.
-  res = pixy.line.getMainFeatures();
-  Serial.println(res);
-  
-  if (res == 1) {
-      if (pixy.line.vectors->m_x1 > X_CENTER) {
-          left = 75;
-          right = 50;
-          Serial.println("RIGHT");
-      } else if (pixy.line.vectors->m_x1 < X_CENTER) {
-          left = 50;
-          right = 75;
-          Serial.println("LEFT");
-      } else {
-          left = 75;
-          right = 75;
-          Serial.println("Straight");
-      }
-    } else if(res <= 0) {
-      left = 0;
-      right = 0;
-      Serial.println("stop ");
-    }
+  pixy.changeProg("line");
+  Serial.println("Station Mode");
   leftMotor -> run(FORWARD);
-  leftMotor -> setSpeed(left);
+  leftMotor -> setSpeed(0);
   rightMotor -> run(FORWARD);
-  rightMotor -> setSpeed(right);
-  Serial.println(left);
-  Serial.println(right);
-  //delay(2000);
-
-  if (res&LINE_BARCODE){
-      if (pixy.line.barcodes->m_code==0){
-        leftMotor -> run(RELEASE);
-        rightMotor -> run(RELEASE);
-        Serial.println("Delivered");
-        delay(10000);
+  rightMotor -> setSpeed(0);
+  Serial.println(res);
+  delay(2000);
+  pixy.line.barcodes -> print();
+  delay(2000);
+  Serial.println(pixy.line.barcodes -> m_code);
+  if (LINE_BARCODE){
+      if (pixy.line.barcodes->m_code==33){
+        Serial.println("Found Station 1");
+        //Turn 90 degrees to the right
       }
-    }
-
-    */
-    
+  } 
+  delay(5000);
+  pixy.changeProg("color");
 }
